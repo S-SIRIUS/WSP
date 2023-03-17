4ecc1ca331a731625ec8b7e1591813842e136150
unknown <sirius5b8bgmail.com>
2023-03-18 03:03:21 +0900

    Modified Rainbow

--- Rainbowtable.py
+++ Rainbowtable.py
@@ -7,8 +7,8 @@ def makehash(ans):
         for j in range(0, 500):
             sha = hashlib.sha1(sha.encode()).hexdigest()
         if(sha==ans):
-            return i
+            return str(i)
 
-original=makehash("df8d533aa927cf060f44aefb88a1a5d2066681dc")
+original=makehash("1a46f151b683accf058f2335c4e5c9b3b2ccef21")
 original = original+"salt_for_you"
 print(original)
