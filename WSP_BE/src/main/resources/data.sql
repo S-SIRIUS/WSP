e97600799373e829c6721bcf56c453f09846d7c3
S-SIRIUS <sirius5b8b@gmail.com>
2024-07-15 21:09:51 +0900

    [fix] testsql

--- WSP_BE/src/main/resources/data.sql
+++ WSP_BE/src/main/resources/data.sql
@@ -1,6 +1,6 @@
 use wsp;
-INSERT INTO user (id, email, password) VALUES (1, 'test@naver.com', '1234');
-INSERT INTO user_info (id, u_id, total_points, ranking, introduction) VALUES(1, 1, 100, 1, 'hi im first solver');
+INSERT INTO user_info (id, total_points, ranking, introduction) VALUES(1, 100, 1, 'hi im first solver');
+INSERT INTO user (id, email, password, info_id) VALUES (1, 'test@naver.com', '1234', 1);
 INSERT INTO article(id, title, content, w_date, u_date, u_id) VALUES(1, 'hi', 'i have a question', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP,1);
 INSERT INTO comment(id, a_id, content, w_date, u_date) VALUES(1, 1, 'wow amazing', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 INSERT INTO problem(id, points) VALUES(1, 100);
